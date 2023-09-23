d35db760661678685455278e4349feb0161f1505
Jorge  Alvarado <coco0340@hotmail.com>
2023-09-22 19:00:46 -0500

    add backend

---
+++ backend.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      #version = "~> 4.12"
+      version = ">= 4.65"
+     }
+  }
+  # Adding Backend as S3 for Remote State Storage
+  backend "s3" {
+    bucket = "pruebaterraform"
+    key    = "terraform.tfstate"
+    region = "us-west-2"     
+  }  
+} 
